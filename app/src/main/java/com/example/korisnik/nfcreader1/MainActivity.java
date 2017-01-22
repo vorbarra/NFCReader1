f0f1f99b15b067954894afba48767d36832b988b
Pale <pporob00@fesb.hr>
2017-01-22 18:56:09 +0100

    Syncano

--- app/src/main/java/com/example/korisnik/nfcreader1/MainActivity.java
+++ app/src/main/java/com/example/korisnik/nfcreader1/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.korisnik.nfcreader1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import be.appfoundry.nfclibrary.activities.NfcActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity  extends NfcActivity {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        for (String message : getNfcMessages()){
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+        }
     }
 }
